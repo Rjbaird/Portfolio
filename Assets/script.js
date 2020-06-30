ed314be8cd69565101c81148334cf8d47bdbccf3
Ryan Baird <rjbaird09@gmail.com>
2020-06-30 11:36:26 -0500

    added downloadable pdf resume

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// $.ajax({
+//     url: "https://api.github.com/repos/VonC/gitolite/git/refs/tags",
+//     dataType: "json",
+//     success: function (returndata)
+//     {
+//     console.log(returndata)
+//     }  
+// });
